5379e45c5b0b081788b17cbc8ff1edbaf1bb2cce
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-04-26 16:27:47 +0200

    Add and complete index.html and main.js

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/* RICHIESTA
+Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21 */
+
+
+
+// Dichiaro le mie VARIABILI
+let userName, userSurname, userColor, messaggio;
+
+// Chiedo all'utente il proprio NOME
+userName = prompt("Come ti chiami?");
+console.log(userName);
+
+// Chiedo all'utente il proprio COGNOME
+userSurname = prompt("Qual è il tuo cognome?");
+console.log(userSurname);
+
+// Chiedo all'utente il proprio COLORE PREFERITO?
+userColor = prompt("E il tuo colore preferito?");
+console.log(userColor);
+
+// Creo MESSAGGIO per l'utente
+messaggio = `La tua password potrebbe essere ${userName+userSurname+userColor+21}`;
+console.log(messaggio);
+
+// Mostro messaggio a SCHERMO
+document.getElementById("mio_id").innerHTML = messaggio;
